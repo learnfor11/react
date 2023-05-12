afafb6d33f27d398eb97b8c2031b63d7d1dd535a
gy <372301467@qq.com>
2023-05-12 23:31:54 +0800

    feat: v18.2.0

---
+++ react-dom.development.js
@@ -0,0 +1,2 @@
+import 'https://unpkg.com/react-dom@18.2.0/umd/react-dom.development.js'
+export default ReactDOM
---
+++ react.development.js
@@ -0,0 +1,18 @@
+import 'https://unpkg.com/react@18.2.0/umd/react.development.js'
+
+export const Component = React.Component
+export const Fragment = React.Fragment
+export const Suspense = React.Suspense
+export const createContext = React.createContext
+export const createElement = React.createElement
+export const createRef = React.createRef
+export const isValidElement = React.isValidElement
+export const useCallback = React.useCallback
+export const useContext = React.useContext
+export const useEffect = React.useEffect
+export const useLayoutEffect = React.useLayoutEffect
+export const useMemo = React.useMemo
+export const useRef = React.useRef
+export const useState = React.useState
+
+export default React
